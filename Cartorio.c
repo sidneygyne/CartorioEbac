8c737157c4e4d390f83fb8411da503508f536985
sidneygyne <155782406+sidneygyne@users.noreply.github.com>
2024-01-05 19:53:24 -0300

    Opção de saida
    Incluído a opção de saída do usuário do sistema

--- Cartorio.c
+++ Cartorio.c
@@ -138,7 +138,8 @@ int main()
 		printf("Escolha a opção desejada do menu: \n\n"); // \n: para pular linha
 		printf("\t1 - Registrar nomes\n");		// \t : dar um espaço tipo TAB
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); // Fim do menu
 		//printf("Esse Software é de livre uso dos alunos\n");
 	
@@ -160,6 +161,11 @@ int main()
 			deletar(); //chamada de funcões
 			break;
 			
+			case 4:
+			printf("Obrigado por ultilizar o sistena!\n");
+			return 0;
+			break;
+				
 			default:	
 			printf("Essa opção nao está disponivel!\n");
 			system("pause");
